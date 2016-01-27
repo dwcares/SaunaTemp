7f827fc8d2d035c22b439770f99ef0db9f99111a
David Washington <dwashin@outlook.com>
2016-01-27 14:19:41 -0600

    Text when its on or hot

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var spark = require('spark');
+var sms = require("twilio")(process.env.TwilioAccountSID, process.env.TwilioAccessToken);
+var lastPublish;
+var lastPublishHot;
 
 app.get('/', function(req, res) {
  res.sendFile(__dirname + '/public/index.html');
@@ -15,11 +18,22 @@ http.listen(port, function() {
 });
 
 spark.on('login', function() {
+    
     spark.getEventStream('tempf', process.env.ParticleDeviceId, function(msg) {
         if (msg) {
             console.log("Event: " +msg.name +":" + msg.data);
-            io.emit('tempf', msg.data);      
-        }
+            io.emit('tempf', msg.data);    
+            
+            if (!lastPublish || Date.now() - lastPublish > 3600000) {
+                sendSMS("Sauna's on!");
+                lastPublish = Date.now();
+            }
+            
+            if (parseInt(msg.data) > 160 && (!lastPublishHot ||  Date.now() - lastPublishHot > 3600000 )) {
+                sendSMS("Sauna's hot and ready!");
+                lastPublishHot = Date.now();
+            }
+        }   
     });
     
     spark.getEventStream('spark/status', process.env.ParticleDeviceId, function(msg) {
@@ -31,3 +45,13 @@ spark.on('login', function() {
 });
 
 spark.login({ accessToken: process.env.ParticleAccessToken });
+
+function sendSMS(message) {
+        sms.sendMessage({
+            to: process.env.SMSNumber, 
+            from: process.env.TwilioPhoneNumber, 
+            body: message
+        }, function(err, boo) {
+            console.log("SMS Err: " + err);
+        });
+}
