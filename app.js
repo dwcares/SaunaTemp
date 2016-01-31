418280c49d32743a7e1cccab8856be632ee58b50
David Washington <dwashin@outlook.com>
2016-01-31 12:48:52 -0600

    clean up ui, better initial state

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var spark = require('spark');
-var sms = require("twilio")(process.env.TwilioAccountSID, process.env.TwilioAccessToken);
 var lastPublish;
-var lastPublishHot;
+var lastTemp;
 
 app.get('/', function(req, res) {
  res.sendFile(__dirname + '/public/index.html');
@@ -19,39 +18,40 @@ http.listen(port, function() {
 
 spark.on('login', function() {
     
+    spark.getDevice(process.env.ParticleDeviceId, function(err, device) {
+        if (err) {
+            console.log("Error getting device: " + err);
+            return;
+        }
+        
+        console.log("Device connected: "  + device.connected);
+        
+        device.getVariable('tempf', function(err, data) {
+            if (err) {
+                console.log("Error getting initial temp: " + err);
+                return;
+            }
+        
+            console.log("Current temp: " + data.result);
+            lastTemp = data.result;
+        });
+    });
+    
+    io.on('connection', function(socket){
+        console.log('a user connected');
+        socket.emit('tempf', {tempf: lastTemp, updated: lastPublish});    
+    });
+    
     spark.getEventStream('tempf', process.env.ParticleDeviceId, function(msg) {
         if (msg) {
             console.log("Event: " +msg.name +":" + msg.data);
-            io.emit('tempf', msg.data);    
             
-            if (!lastPublish || Date.now() - lastPublish > 3600000) {
-                sendSMS("Sauna's on!");
-                lastPublish = Date.now();
-            }
+            lastTemp = msg.data;
+            lastPublish = Date.now();
             
-            if (parseInt(msg.data) > 160 && (!lastPublishHot ||  Date.now() - lastPublishHot > 3600000 )) {
-                sendSMS("Sauna's hot and ready!");
-                lastPublishHot = Date.now();
-            }
+            io.emit('tempf',  {tempf: lastTemp, updated: lastPublish});    
         }   
-    });
-    
-    spark.getEventStream('spark/status', process.env.ParticleDeviceId, function(msg) {
-        if (msg) {
-            console.log("Device status: " + msg.data);
-            io.emit('devicestatus', msg.data);
-        }
     });
 });
 
 spark.login({ accessToken: process.env.ParticleAccessToken });
-
-function sendSMS(message) {
-        sms.sendMessage({
-            to: process.env.SMSNumber, 
-            from: process.env.TwilioPhoneNumber, 
-            body: message
-        }, function(err, boo) {
-            console.log("SMS Err: " + err);
-        });
-}
